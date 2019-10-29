e2de11eed23d704bc6ffd08381bdb946d7b575fc
Roberto Campos <robertoccamposn@gmail.com>
2019-10-29 02:13:25 -0400

    Implementa webpack
    utiliza loaders pra babel e sass

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		filename: 'main.js',
+		publicPath: '/',
+	},
+	devServer: {
+		historyApiFallback: true
+	 },
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_module/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.(css|sass|scss)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpg|jpeg|svg|ico|gif)?$/,
+				use: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve( __dirname, 'public/index.html' ),
+			filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles.css',
+		}),
+	]
+};

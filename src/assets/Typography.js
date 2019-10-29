ca6b7b1b39e39ede6f3564fa45fef7fb17473fc5
Roberto Campos <robertoccamposn@gmail.com>
2019-10-29 02:15:47 -0400

    Implementa base visual
    Adiciona cores, tipografica e sass ao projeto com responsividade mobile e desktop

---
+++ src/assets/Typography.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const H3 = styled.h3`
+  font-size: 1em;
+  margin-top: 1.33em;
+  margin-bottom: 1.33em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+`;
+
+const P = styled.p`
+  margin-top: 1em;
+  margin-bottom: 1em;
+  margin-left: 0;
+  margin-right: 0;
+  font-size: 0.85em;
+`;
+
+export { H3, P };
